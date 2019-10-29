1ea5f2bdb6a2f864bb826b1437a30f34658805c6
Keerthi Mani <carkeane.getz@gmail.com>
2019-10-29 17:55:48 -0400

    Supervisor updates with company's profit

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -21,16 +21,16 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
 });
 
+// prompt for supervisor either to view product sales or create new department 
 function menuOptions() {
     inquirer.prompt([
         {
             type: "list",
             name: "options",
             message: "Select any options from the menu : ",
-            choices: ["View Product Sales by Department", "Create New Department"]
+            choices: ["View Product Sales by Department", "Create New Department", "Quit"]
         }
     ]).then(function (supervisorChoice) {
         switch (supervisorChoice.options) {
@@ -41,33 +41,61 @@ function menuOptions() {
             case "Create New Department":
                 createNewDepartment();
                 break;
+            case "Quit":
+                exit();
+                break;
         }
     });
 }
 
 //Supervisor views the product sales by department
 function viewProductDepartment() {
-    //prints the items for sale and their details
-    connection.query("SELECT * FROM departments", function (err, res) {
+    connection.query("SELECT d.department_id, d.department_name, d.over_head_costs, p.product_sales * p.price as 'Product_Sales', (p.product_sales * p.price) - d.over_head_costs AS Profit FROM departments d, products p WHERE p.department_name = d.department_name ORDER BY department_id ASC", function (err, results) {
         if (err) throw err;
-        console.log("\n--------------------------------------------");
-
-        for (var i = 0; i < res.length; i++) {
-            console.table(
-                {
-                    department_id: res[i].department_id,
-                    over_head_costs: res[i].over_head_costs
-                }
-            );
-        }
-        console.log("\n--------------------------------------------");
+        //console.log(results);
+        console.log('-----------------------------------------------------------------------');
+        console.log('____________.~"~._.~"~._.~Welcome to BAMazon~._.~"~._.~"~._____________');
+        console.log('-----------------------------------------------------------------------');
+        console.table(results);
         menuOptions();
     });
-}
+};
 
 //Supervisor creates new department
 function createNewDepartment() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department_name",
+            message: "Enter a new department name :"
+        },
+        {
+            type: "input",
+            name: "over_head_cost",
+            message: "Enter overall head cost of the product :"
+        }
+    ]).then(function (supervisorAdds) {
+        connection.query("INSERT INTO departments SET ?", {
+            department_name: supervisorAdds.department_name,
+            over_head_costs: supervisorAdds.over_head_cost
+        }, function (err, res) {
+            if (err) throw err;
+            console.log("\n------------------------------------------------");
+            console.log("\nNew department is added");
+            console.log("\n------------------------------------------------");
 
+        });
+        connection.query("SELECT * FROM departments", function (err, res) {
+            if (err) throw err;
+            console.table(res);
+        });
+        menuOptions();
+    });
 
+}
+
+function exit() {
+    connection.end();
+    console.log("SEE YOU AGAIN!")
 }
 menuOptions();
